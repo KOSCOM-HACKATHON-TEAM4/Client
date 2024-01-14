733be84737585a292dc145d6949817325f20fe09
lsjpjs1 <lsjpjs1@naver.com>
2024-01-14 15:01:07 +0900

    chore: 웹서버 배포 테스트2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          배포 테스트
+          배포 테스트222
         </p>
         <a
           className="App-link"
